262450f9b64984ebc2d540a759e3d6422d6a0768
Luis Paulo Lessa <prof.lpjunior@gmail.com>
2019-12-05 21:56:31 -0300

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,13 +1,13 @@
 <?php
 
-    $nome = $_POST['nome_form'];
-    $email = $_POST['email_form'];
-    $senha = $_POST['senha_form'];
+    $nome = addslashes($_POST['nome_form']);
+    $email = addslashes($_POST['email_form']);
+    $senha = addslashes($_POST['senha_form']);
 
     $link = mysqli_connect("localhost","root","","db_test", 3306);
     mysqli_set_charset($link, "utf8");
 
-    $sql = "insert into tb_usuarios (nome, email, senha) values ('" . $nome . "', '" . $email ."', '" . $senha. "')";
+    $sql = "insert into tb_usuarios (nome, email, senha) values ('{$nome}', '{$email}', md5('{$senha}'))";
   
     $verifica = mysqli_query($link, $sql) or die("erro ao inserir");
 
@@ -19,7 +19,7 @@
         die();
     }else{
         echo "<script language='javascript' type='text/javascript'>
-            alert('');
+            alert('Cadastrado com sucesso!');
             window.location.href='index.html';
             </script>";
         die();
